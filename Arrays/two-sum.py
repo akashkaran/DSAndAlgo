103eaf6747c81914f6d73457fdb43f57f9ca3c3b
Akash Karan <karanakash8@gmail.com>
2021-04-25 17:22:33 +0530

    arrays: two sum

---
+++ Arrays/two-sum.py
@@ -0,0 +1,18 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+# target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+from typing import List
+
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    di = {}
+    for i in range(len(nums)):
+        if target - nums[i] in di:
+            return [di[target - nums[i]], i]
+        else:
+            di[nums[i]] = i
+    return []
